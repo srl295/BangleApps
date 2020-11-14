41e16e1d16f666a87c82123dc69c9980939afc2f
Steven R. Loomis <srl295@gmail.com>
2020-11-14 11:59:17 -0600

    ctxtimer: rewrite, works but no persist

--- apps/ctxtimer/app.js
+++ apps/ctxtimer/app.js
@@ -1,181 +1,81 @@
-let counter = 0;
-let setValue = 0;
-let counterInterval, alarmInterval, buzzInterval;
-let state;
-let saved = require("Storage").readJSON("ctxtimer.json",true) || {};
-
-const DEBOUNCE = 50;
-
-function buzzAndBeep() {
-  buzzInterval = -1;
-  return Bangle.buzz(1000, 1)
-    .then(() => Bangle.beep(200, 3000))
-    .then(() => {
-      if (buzzInterval==-1)
-        buzzInterval = setTimeout(buzzAndBeep, 5000);
-    });
+//let saved = require("Storage").readJSON("ctxtimer.json",true) || {};
+let saved = {};
+function save() {
+//    require("Storage").write("ctxtimer.json", saved);
 }
 
-function outOfTime() {
-  g.clearRect(0, 0, 220, 70);
-  g.setFontAlign(0, 0);
-  g.setFont("6x8", 3);
-  g.drawString("Time UP!", 120, 50);
-  counter = setValue;
-  buzzAndBeep();
-  if (alarmInterval) clearInterval(alarmInterval);
-  alarmInterval = setInterval(() => {
-    g.clearRect(0, 70, 220, 160);
-    setTimeout(draw, 200);
-  }, 400);
-  state = "stopped";
+function now() {
+  return new Date().getTime();
 }
 
-function draw() {
-  const minutes = Math.floor(counter / 60);
-  const seconds = Math.floor(counter % 60);
-  const seconds2Digits = seconds < 10 ? `0${seconds}` : seconds.toString();
-  g.clearRect(0, 70, 220, 160);
-  g.setFontAlign(0, 0);
-  g.setFont("6x8", 7);
-  g.drawString(
-    `${minutes < 10 ? "0" : ""}${minutes}:${seconds2Digits}`,
-    120,
-    120
-  );
-}
+let lastStart, lastEnd, start, end;
 
-function countDown() {
-  if (counter <= 0) {
-    if (counterInterval) {
-      clearInterval(counterInterval);
-      counterInterval = undefined;
+E.showMessage('ctx timer');
+
+// 1: start
+setWatch(() => {
+  if(!start || end) {
+    lastEnd = end;
+    lastStart = start;
+    end = null;
+  } // else: just restarting
+  start = now();
+}, BTN1, {repeat:true});
+
+// 2: end
+setWatch(() => {
+  if(start && !end) {
+    end = now();
+  }
+}, BTN2, {repeat:true});
+
+// 3: cancel
+setWatch(() => {
+  if(start) {
+    start = null;
+    if(end) {
+      end = null;
     }
-    outOfTime();
-    return;
   }
+}, BTN3, {repeat:true});
 
-  counter--;
-  draw();
+// interval to string
+function iv2s(a, b, what) {
+  if(!a || !b) return '';
+  const d = (b-a)/1000;
+  return (what||'') + (Number(d.toFixed(0)).toString()+'s');
 }
 
-function clearIntervals() {
-  if (alarmInterval) clearInterval(alarmInterval);
-  if (counterInterval) clearInterval(counterInterval);
-  if (buzzInterval>0) clearTimeout(buzzInterval);
-  alarmInterval = undefined;
-  counterInterval = undefined;
-  buzzInterval = undefined;
+
+function twodigit(a) {
+  if(a>=10) return Number(a).toString();
+  return '0' + Number(a).toString();
 }
 
-function set(delta) {
-  if (state === "started") return;
-  counter += delta;
-  saved.counter = counter;
-  require("Storage").write("ctxtimer.json", saved);
-  if (state === "unset") {
-    state = "set";
-  }
-  draw();
-  g.flip();
+function walltime(a, what) {
+  if(!a) return '';
+  const d = new Date(a);
+  const h = twodigit(d.getHours());
+  const m = twodigit(d.getMinutes());
+  const s = twodigit(d.getSeconds());
+  return `${what||''}${h}:${m}:${s}`;
 }
 
-function startTimer() {
-  setValue = counter;
-  countDown();
-  counterInterval = setInterval(countDown, 1000);
-}
-
-// unset -> set -> started -> -> stopped -> set
-const stateMap = {
-  set: () => {
-    state = "started";
-    startTimer();
-  },
-  started: () => {
-    resetTimer(setValue);
-  },
-  stopped: () => {
-    resetTimer(setValue);
-  }
-};
-
-function changeState() {
-  if (stateMap[state]) stateMap[state]();
-  drawLabels();
-  draw();
-}
-
-function drawLabels() {
+setInterval(() => {
   g.clear();
-  g.setFontAlign(-1, 0);
-  g.setFont("6x8", 7);
-  if (state != "started") // only when not runnung
-    g.drawString(`+  +`, 35, 180);
-  g.setFontAlign(0, 0, 3);
-  g.setFont("6x8", 1);
-  g.drawString("Cancel                   Start", 230, 120);
-  if (state != "started") // only when not runnung
-    g.drawString("Back", 230, 120);
-}
-
-function resetTimer(value) {
-  clearIntervals();
-  VIBRATE.reset(); // turn off vibration (clearIntervals stops the buzz turning off)
-  counter = value;
-  setValue = value;
-  drawLabels();
-  draw();
-  state = value === 0 ? "unset" : "set";
-}
-
-function addWatch() {
-  clearWatch();
-  setWatch(changeState, BTN1, {
-    debounce: DEBOUNCE,
-    repeat: true,
-    edge: "falling"
-  });
-  setWatch(() => {
-    if (state !== "started") {
-      Bangle.showLauncher();
-    }},
-  BTN2,
-  {
-    repeat: false,
-    edge: "falling",
-  });
-  setWatch(
-    () => {
-      resetTimer(0);
-    },
-    BTN3,
-    {
-      debounce: DEBOUNCE,
-      repeat: true,
-      edge: "falling"
-    }
-  );
-  setWatch(
-    () => {
-      set(60);
-    },
-    BTN4,
-    {
-      debounce: DEBOUNCE,
-      repeat: true,
-      edge: "falling"
-    }
-  );
-  setWatch(() => set(1), BTN5, {
-    debounce: DEBOUNCE,
-    repeat: true,
-    edge: "falling"
-  });
-}
-Bangle.on("aiGesture", gesture => {
-  if (gesture === "swipeleft" && state === "stopped") resetTimer(0);
-});
-
-resetTimer(saved.counter || 0);
-addWatch();
+  const n = now();
+  
+  const stats = [
+    walltime(n, ''),
+    walltime(start, '@:'),
+    iv2s(start, end||n, 'Dur:'),
+    walltime(lastStart, 'P@:'),
+    iv2s(lastStart, lastEnd, 'PDur:'),
+    iv2s(lastEnd, start, 'Freq:'),
+    iv2s(end, n, 'Quiet:')
+  ].join('\n');
+    
+  E.showMessage('1: start\n2: stop\n3: cancel\n----\n' + stats);
+  g.flip();
+  
+}, 1000);
